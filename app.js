30bc3301dd8253ec40b498d11c1d9c870d8de014
Michele <mstewart2@ufl.edu>
2024-08-06 13:04:46 -0400

    make copyright year dynamic

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-function copyrightYear() {
+$(document).ready(function() {
+     
+});
+$(function() {
     var d = new Date();
     var y = d.getFullYear();
     document.getElementById("copyright").innerHTML = '&copy; ' + y + ' Rehab Reps All Rights Reserved';
- }
- 
- copyrightYear();
+
+});
